465b933ae29fa8b5f9f44d69c775723fd4c1af8d
astro2049 <jerryloi@163.com>
2021-04-11 23:24:29 +0800

    allow cross-origin requests on localhost

---
+++ chat/src/main/java/star/astro/chat/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package star.astro.chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+
+}
