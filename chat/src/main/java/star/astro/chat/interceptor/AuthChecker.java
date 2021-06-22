b7062b11ebe305943f88531f5244a2ec66af51f7
astro2049 <jerryloi@163.com>
2021-06-22 17:16:11 +0800

    feat: server - complete auth logic

--- chat/src/main/java/star/astro/chat/interceptor/AuthChecker.java
+++ chat/src/main/java/star/astro/chat/interceptor/AuthChecker.java
@@ -1,8 +1,10 @@
 package star.astro.chat.interceptor;
 
+import com.alibaba.fastjson.JSONObject;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import star.astro.chat.model.mongodb.User;
 import star.astro.chat.repository.UserRepository;
@@ -10,7 +12,9 @@ import star.astro.chat.util.JwtTokenUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
+@Component
 public class AuthChecker implements HandlerInterceptor {
 
     @Autowired
@@ -20,6 +24,9 @@ public class AuthChecker implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (request.getMethod().equals("OPTIONS")) {
+            return true;
+        }
         boolean granted = false;
         String token = request.getHeader("token");// 从 http 请求头中取出 token
         if (token != null) {
@@ -35,7 +42,17 @@ public class AuthChecker implements HandlerInterceptor {
 
             }
         }
-        return granted;
+        if (granted) {
+            return true;
+        } else {
+            JSONObject ret = new JSONObject();
+            ret.put("message", "unauthorized request");
+            PrintWriter out = response.getWriter();
+            out.write(ret.toString());
+            out.flush();
+            out.close();
+            return false;
+        }
     }
 
 }
