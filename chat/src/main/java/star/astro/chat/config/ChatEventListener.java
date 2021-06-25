350ad5422ac42ddaee44a47bf871399043b32459
astro2049 <jerryloi@163.com>
2021-06-25 23:37:08 +0800

    feat: complete Stomp session related logic (finally)!
    and Chat.js layout updates

--- chat/src/main/java/star/astro/chat/config/ChatEventListener.java
+++ chat/src/main/java/star/astro/chat/config/ChatEventListener.java
@@ -4,29 +4,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import star.astro.chat.model.mongodb.StompSession;
+import star.astro.chat.repository.StompSessionRepository;
 import star.astro.chat.service.UserService;
-
-import java.util.Objects;
 
 @Component
 public class ChatEventListener {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private StompSessionRepository stompSessionRepository;
+
+    // not perfect to create session here
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+        try {
+            String username = headerAccessor.getNativeHeader("username").get(0);
+            StompSession stompSession = new StompSession(sessionId, username);
+            stompSessionRepository.save(stompSession);
+            userService.userOnline(username);
+            System.out.println(username + " is online");
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 
     @EventListener
-    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+    public void handleWebSocketConnectedListener(SessionConnectedEvent event) {
         System.out.println("Received a new web socket connection");
     }
 
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
         try {
-            String username = (String) Objects.requireNonNull(headerAccessor.getSessionAttributes()).get("username");
+            StompSession stompSession = stompSessionRepository.findStompSessionBySessionId(sessionId);
+            String username = stompSession.getUsername();
+            stompSessionRepository.delete(stompSession);
             userService.userOffline(username);
+            System.out.println(username + " is offline");
         } catch (NullPointerException e) {
             System.out.println(e.getMessage());
         }
