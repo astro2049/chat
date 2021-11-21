b7e67d8b4b7f10d54e1b296623c1c589426db962
Jerry Loi <45759373+astro2049@users.noreply.github.com>
2021-11-21 13:23:07 +0800

    feat: update chatting
    chat message structure update
    front - findChatMessages logic update

--- chat/src/main/java/star/astro/chat/controller/NotificationController.java
+++ chat/src/main/java/star/astro/chat/controller/NotificationController.java
@@ -6,11 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import star.astro.chat.service.NotificationService;
 
 import javax.servlet.http.HttpServletRequest;
-import java.net.http.HttpRequest;
 import java.util.Map;
 
 @Controller
@@ -23,7 +21,6 @@ public class NotificationController {
     @PostMapping("/new-friend")
     public ResponseEntity<?> noticeUserOfNewFriend(@RequestBody Map<String, Object> params, HttpServletRequest request) {
         String username = (String) params.get("name");
-        System.out.println(username);
         notificationService.noticeUserOfNewChatroom(username);
 
         return ResponseEntity.noContent().build();
